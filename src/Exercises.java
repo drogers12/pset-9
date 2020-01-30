6b4bc7b89adbe1b06e9d642ce10e8772d6f5a8db
David Rogers <drogers@ucvts.tec.nj.us>
2020-01-30 09:10:43 -0500

    difference

--- src/Exercises.java
+++ src/Exercises.java
@@ -15,6 +15,7 @@ public class Exercises {
 	}
 	
 	public ArrayList<String> endsMeet(ArrayList<String> values, int n) {
+		// write your code here
 
 		ArrayList<String> empty = new ArrayList<String>();
 		if (values == null || values.size() < n || n <= 0) {
@@ -38,7 +39,23 @@ public class Exercises {
 	public int difference(ArrayList<Integer> numbers) {
 		// write your code here
 		
-		return -1;		// default return value to ensure compilation
+		if (numbers == null || numbers.size() < 1) {
+			return -1;
+		} else {
+			int max = numbers.get(0);
+			int min = numbers.get(0);
+		
+			for (int i = 1; i < numbers.size(); i++) {
+				if (numbers.get(i) > max) {
+					max = numbers.get(i);
+				}
+				if (numbers.get(i) < min) {
+					min = numbers.get(i);
+				}
+			}
+			return max-min;
+		}
+				// default return value to ensure compilation
 	}
 	
 	public double biggest(ArrayList<Double> numbers) {
